112756ae50e06ba999c1dc986fc2ff08cbf554f3
chengfang <1679997020@qq.com>
2022-11-18 00:23:19 +0800

    stash

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,32 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { CARD_LIST_TYPE } from '@/data/type'
 import './index.scss'
 
 import ContentCard from './components/ContentCard'
 
+const baseList: Array<CARD_LIST_TYPE> = [
+	{ title: '待办',  show: false },
+	{ title: '进行中', show: false },
+	{ title: '完成', show: false }
+]
 const Index = () => {
-	const baseList: Array<CARD_LIST_TYPE> = [
-		{ title: '待办',  show: false },
-		{ title: '进行中', show: false },
-		{ title: '完成', show: false }
-	]
 	const [cardList, setCardList] = useState<Array<CARD_LIST_TYPE>>(baseList)
+	console.log('111111111111111111111', cardList)
 
 	const handleAdd = () => { // add card to list
 		setCardList(res => [ ...res, { title: 'test_add', show: false }])
 	}
 	const handleCardChange = (val: CARD_LIST_TYPE, index: number, show?: boolean) => {
-		console.log(cardList)
-		let newCard = [...cardList]
-		newCard[index] = val
-		if (show) {
-			newCard = newCard.map((item, i) => {
-				if (i === index) return { ...item, show: true }
-				else return {...item, show: false }
-			})
-		}
-		console.log(newCard)
-		setCardList(newCard)
+		console.log('222222222222222222', cardList)
+		// const newCard = [...cardList]
+		// newCard[index] = val
+		const newCard = cardList.map((item, i) => {
+			if (i === index) return { ...val, show: true }
+			else return {...item, show: false }
+		})
+		setCardList('33333333333333333', newCard)
 	}
 
   return <div className='pc-board'>

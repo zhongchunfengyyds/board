f35ca9af89393ff802fee4f4b099459fa1858bf8
仲么了 <zhongchunyang@rocan.com.cn>
2022-11-23 10:02:42 +0800

    页面切图

--- src/pages/index/components/CardDetailModal/index.tsx
+++ src/pages/index/components/CardDetailModal/index.tsx
@@ -1,5 +1,12 @@
 import React, {FC, useMemo, ChangeEvent, useEffect, useState, memo} from 'react'
-import {Mentions} from 'antd'
+import {Mentions, Button, Checkbox, Col, Row, Avatar, List} from 'antd'
+import {
+    CreditCardOutlined,
+    MenuUnfoldOutlined,
+    OrderedListOutlined,
+    CommentOutlined,
+    CloseOutlined,
+} from '@ant-design/icons'
 const {Option} = Mentions
 import BoardModal from '@/components/BoardModal'
 import './index.scss'
@@ -14,35 +21,10 @@ const Index: FC<PropsType> = ({show, id, onClose}) => {
     const titleDom = useMemo(() => {
         return (
             <div className="content-left-item">
-                <img src="" alt="" />
+                <CreditCardOutlined />
                 <div className="right">
                     <h3>标题</h3>
                     <div className="desc">介绍</div>
-                    <div className="tag">
-                        <div className="tag-title">标签</div>
-                        <div className="tag-content">
-                            <div className="tag-content-item success">
-                                <div className="tag-content-item-point"></div>
-                                成功
-                            </div>
-                            <div className="tag-content-item warning">
-                                <div className="tag-content-item-point"></div>
-                                警告
-                            </div>
-                            <div className="tag-content-item error">
-                                <div className="tag-content-item-point"></div>
-                                错误
-                            </div>
-                            <div className="tag-content-item primary">
-                                <div className="tag-content-item-point"></div>
-                                主要
-                            </div>
-                            <div className="tag-content-item stop">
-                                <div className="tag-content-item-point"></div>
-                                停止
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         )
@@ -51,7 +33,7 @@ const Index: FC<PropsType> = ({show, id, onClose}) => {
     const DesDOm = useMemo(() => {
         return (
             <div className="content-left-item">
-                <img src="" alt="" />
+                <MenuUnfoldOutlined />
                 <div className="right">
                     <div className="title">描述</div>
                     <div className="pt10"></div>
@@ -66,66 +48,92 @@ const Index: FC<PropsType> = ({show, id, onClose}) => {
     const listDom = useMemo(() => {
         return (
             <div className="content-left-item">
-                <img src="" alt="" />
+                <OrderedListOutlined />
                 <div className="right">
                     <div className="title">清单</div>
-                    <div className="pt10"></div>
-                    <div className="checkbox">
-                        <input
-                            type="checkbox"
-                            name="清单"
-                            id="checkbox1"
-                            className="checkbox-input cursor-pointer"
-                        />
-                        <label
-                            htmlFor="checkbox1"
-                            className="ml5 checkbox-label cursor-pointer">
-                            哈哈哈
-                        </label>
-                    </div>
-                    <div className="checkbox">
-                        <input
-                            type="checkbox"
-                            name="清单"
-                            className="checkbox-input cursor-pointer"
-                        />
-                        <label
-                            className="ml5 checkbox-label  cursor-pointer"
-                            cursor-pointer>
-                            哈哈哈
-                        </label>
-                    </div>
-                    <div className="checkbox">
-                        <input
-                            type="checkbox"
-                            name="清单"
-                            className="checkbox-input cursor-pointer"
-                        />
-                        <label
-                            className="ml5 checkbox-label  cursor-pointer"
-                            cursor-pointer>
-                            哈哈哈
-                        </label>
-                    </div>
+                    <div className="pt12"></div>
+                    <Checkbox.Group style={{width: '100%'}}>
+                        <Row gutter={[0, 10]}>
+                            <Col span={24}>
+                                <Checkbox value="A">A</Checkbox>
+                            </Col>
+                            <Col span={24}>
+                                <Checkbox value="A">A</Checkbox>
+                            </Col>
+                            <Col span={24}>
+                                <Checkbox value="A">A</Checkbox>
+                            </Col>
+                        </Row>
+                    </Checkbox.Group>
                 </div>
             </div>
         )
     }, [id])
 
+    const data = [
+        {
+            title: 'Ant Design Title 1',
+        },
+        {
+            title: 'Ant Design Title 1',
+        },
+        {
+            title: 'Ant Design Title 1',
+        },
+        {
+            title: 'Ant Design Title 1',
+        },
+        {
+            title: 'Ant Design Title 2',
+        },
+        {
+            title: 'Ant Design Title 3',
+        },
+        {
+            title: 'Ant Design Title 4',
+        },
+    ]
     const CommentDom = useMemo(() => {
         return (
             <div className="content-left-item">
-                <img src="" alt="" />
+                <CommentOutlined />
                 <div className="right">
-                    <div className="title">活动</div>
+                    <div className="title">评论</div>
                     <div className="pt10"></div>
                     <div className="comment">
-                        <Mentions autoSize style={{width: '100%'}}>
+                        <Mentions
+                            autoSize
+                            style={{width: '100%'}}
+                            placeholder="添加评论">
                             <Option value="afc163">afc163</Option>
                             <Option value="zombieJ">zombieJ</Option>
                             <Option value="yesmeck">yesmeck</Option>
                         </Mentions>
-                        <div className="comment-item"> </div>
+                        <div className="mt10"></div>
+                        <Button size="small" type="primary">
+                            评论
+                        </Button>
+                        <div className="mt20"></div>
+                        <List
+                            size="small"
+                            itemLayout="horizontal"
+                            dataSource={data}
+                            renderItem={(item) => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={
+                                            <Avatar src="https://joeschmoe.io/api/v1/random" />
+                                        }
+                                        title={
+                                            <a href="https://ant.design">
+                                                {item.title}
+                                            </a>
+                                        }
+                                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                    />
+                                </List.Item>
+                            )}
+                        />
                     </div>
                 </div>
             </div>
@@ -134,7 +142,14 @@ const Index: FC<PropsType> = ({show, id, onClose}) => {
     return (
         <BoardModal show={show} onClose={onClose}>
             <div className="card-detail-modal">
-                <header>title</header>
+                <header>
+                    <Button
+                        shape="circle"
+                        type="text"
+                        icon={<CloseOutlined />}
+                    />
+                    <Button type="text">拉伸</Button>
+                </header>
                 <div className="content">
                     <div className="content-left">
                         {titleDom}

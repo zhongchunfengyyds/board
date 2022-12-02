a8d9f98d5bc11f0c9334bcc431bba582b37df3e4
kilig <1679997020@qq.com>
2022-12-02 10:29:07 +0800

    获取userid

--- src/hook/useApiIntData.ts
+++ src/hook/useApiIntData.ts
@@ -7,11 +7,12 @@ import { useEffect } from 'react'
 export const useApiInitData = () => {
     const setCardList = useSetCardList()
     const { setUserInfo } = useUserInfo()
-    const { data, error, mutate } = useSWR('oa/tabulated/findByUserId', async () => await apiInitData({
-        userId: '1'
+    const { data: user } = useSWR('org/userResource/userMsg', async () => await apiGetUserInfo())
+    const { data, error, mutate } = useSWR({ url: 'oa/tabulated/findByUserId', args: user }, async () => await apiInitData({
+        userId: user?.data?.user?.id ?? ''
     }))
     useEffect(() => {
-        apiGetUserInfo()
+        console.log(data)
         const resList = data?.data?.result ?? []
         const list = resList.map((item: any, index: number) => {
             return {
